ba4792803e82fa198b6d800a36852c14f575c86a
mamebassine <mamebassine06@gmail.com>
2024-12-11 19:43:28 +0000

    le header et le footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AProposComponent } from './a-propos/a-propos.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ServicesComponent } from './services/services.component';
+// import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+export const routes: Routes = [
+
+  { path: '', component: AccueilComponent },
+  { path: 'a-propos', component: AProposComponent },
+  { path: 'projets', component: ProjetsComponent },
+  { path: 'services', component: ServicesComponent },
+//   { path: 'contact', component: ContactComponent },
+{ path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+
+];
